afe3147d9d793dfa6bda0661e81ab5e4bfea497e
unknown <erik.ostlund@gmail.com>
2024-12-07 11:34:45 +0100

    Added solution for day 03

---
+++ src/Day03.java
@@ -0,0 +1,34 @@
+void main() throws IOException {
+
+    var memory = Files.readString(Path.of("3.txt"));
+
+    var part1 = Pattern.compile("mul\\((\\d+),(\\d+)\\)")
+            .matcher(memory)
+            .results()
+            .mapToInt(r -> Integer.parseInt(r.group(1)) * Integer.parseInt(r.group(2)))
+            .sum();
+
+    System.out.println(part1);
+
+    var results = Pattern.compile("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
+            .matcher(memory)
+            .results()
+            .toList();
+
+    var enabled = true;
+    var part2 = 0;
+
+    for (var result : results) {
+        switch (result.group()) {
+            case "do()" -> enabled = true;
+            case "don't()" -> enabled = false;
+            default -> {
+                if (enabled) {
+                    part2 += Integer.parseInt(result.group(1)) * Integer.parseInt(result.group(2));
+                }
+            }
+        }
+    }
+
+    System.out.println(part2);
+}
